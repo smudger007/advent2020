9df050c6e91862ffb2cf9fd189bdedbf502ea2f2
Mark Smith <smudger007@googlemail.com>
2020-12-03 18:01:29 +0000

    fix pw-valid.py

--- day2/pw-valid.py
+++ day2/pw-valid.py
@@ -1,8 +1,7 @@
 import sys
 import re
 
-def loadPWFromFile():
-    
+def loadPWFromFile():    
     with open("pw-file.txt") as f:
         content = f.read().splitlines()
     f.close()
@@ -16,7 +15,6 @@ def part1(pwlistIn):
         result = pattern.search(pw)
         if result.group(4).count(result.group(3)) >= int(result.group(1)) and result.group(4).count(result.group(3)) <= int(result.group(2)):
             count = count + 1
-
     return count
 
 def part2(pwlistIn):
@@ -25,14 +23,9 @@ def part2(pwlistIn):
 
     for pw in passwords:
         result = pattern.search(pw)
-
-        if (result.group(3) == result.group(4)[result.group(1) - 1]) != (result.group(3) == result.group(4)[result.group(2) - 1]):
-
-        
+        if (result.group(3) == result.group(4)[int(result.group(1)) - 1]) != (result.group(3) == result.group(4)[int(result.group(2)) - 1]):
             count = count + 1
-
     return count
-
 
 #============================================
 # Main Program
@@ -43,7 +36,6 @@ try:
 
     print(f"part one count is {part1(passwords)}")
     print(f"part two count is {part2(passwords)}")
-
 
 except Exception as e:
     print("Aborting..../n", e)
