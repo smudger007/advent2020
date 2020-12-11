4b19221d9eb0dca56a5694f0936c82ed827ae272
Mark Smith <smudger007@googlemail.com>
2020-12-11 23:00:11 +0000

    Added part 2 day 5

--- day5/binary_boarding.py
+++ day5/binary_boarding.py
@@ -7,23 +7,18 @@ def loadInput():
     f.close()
     return content
 
-def scanBoardingPasses(bpIn):
+def getSeatFromBP(bpIn):
+    return (getRowOrCol(bpIn[0:7], 0, 127) * 8) + getRowOrCol(bpIn[7:], 0, 7)
 
+def scanBoardingPasses(bpsIn):
     highestSeat = 0
-    for bp in bpIn:
-        #print(f"BP is {bp}")
-        row = getRowOrCol(bp[0:7], 0, 127)
-        #print(f"row is {row}")
-        col = getRowOrCol(bp[7:], 0, 7)
-        #print(f"col is {col}")
-        seatId = (row * 8) + col
-        #print(f"seatId = {seatId}")
+    for bp in bpsIn:
+        seatId = getSeatFromBP(bp)
         if seatId > highestSeat: highestSeat = seatId
     return highestSeat 
 
 def getRowOrCol(inCode, inStart, inEnd):
     midPoint = inStart + (((inEnd - inStart) + 1) / 2)
-    #print(f"incode = {inCode}  inStart = {inStart} inEnd = {inEnd}  midPoint = {midPoint}")
     if len(inCode) == 1:
         if inCode == 'F' or inCode == 'L': return int(inStart)
         return int(inEnd)
@@ -31,6 +26,13 @@ def getRowOrCol(inCode, inStart, inEnd):
         if inCode[0] == 'F'  or inCode[0] == 'L': 
             return getRowOrCol(inCode[1:], inStart, midPoint - 1)
         return getRowOrCol(inCode[1:], midPoint, inEnd)
+
+def getSeatsOnRowWithEmptySeat(bpIn):
+    rowSeats = [[] for i in range(128)]
+    for bp in bpIn:
+        rowId = getRowOrCol(bp[0:7], 0, 127)
+        rowSeats[rowId].append(getSeatFromBP(bp))
+    return [x for x in rowSeats if len(x) == 7 ]
 
 #============================================
 # Main Program
@@ -40,7 +42,7 @@ try:
     boardingPasses = loadInput()
 
     print(f"Part 1 is {scanBoardingPasses(boardingPasses)}")
-
+    print(f"Part 2 is on this row - {getSeatsOnRowWithEmptySeat(boardingPasses)}")
 
 except Exception as e:
     print("Aborting..../n", e)
