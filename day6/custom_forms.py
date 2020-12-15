da33f39334c0cb6951025dc76bbbd47a69ff5aab
Mark Smith <smudger007@googlemail.com>
2020-12-15 12:39:29 +0000

    Added part 2 code

--- day6/custom_forms.py
+++ day6/custom_forms.py
@@ -7,11 +7,27 @@ def loadInput():
     f.close()
     return content
 
-def scanForms(formIn):
+def scanForms(formsIn):
     count = 0
-    for form in formIn:
+    for group in formsIn:
         holdSet = set()
-        for char in form.replace("\n", ""): holdSet.add(char)
+        for char in group.replace("\n", ""): holdSet.add(char)
+        count = count + len(holdSet)
+    return(count)
+
+def scanFormsPartTwo(formsIn):
+    count = 0
+    for group in formsIn:
+        holdSet = set()
+        for form in group.split("\n"):
+            toRemove = []
+            if len(holdSet) == 0:
+                [holdSet.add(char) for char in form]
+            else:
+                for char in holdSet:
+                    if char not in form: toRemove.append(char)
+                for char in toRemove: holdSet.remove(char)
+                if len(holdSet) == 0: break
         count = count + len(holdSet)
     return(count)
 
@@ -22,6 +38,7 @@ def scanForms(formIn):
 try:
     customForms = loadInput()
     print(f"Part 1 is {scanForms(customForms)}")
+    print(f"Part 2 is {scanFormsPartTwo(customForms)}")
 
 except Exception as e:
     print("Aborting..../n", e)
